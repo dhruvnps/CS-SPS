aa85abb9844e00ecbbc1845a35570a851efb284e
dhruvpattem <dhruv.pattem@gmail.com>
2020-10-02 12:39:21 +0100

    edits

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -15,6 +15,13 @@ namespace Lists
             Console.WriteLine($"Max: {Iterative.Max(list)}");
             Console.WriteLine($"Min: {Iterative.Min(list)}");
             Iterative.Negatives(list).ForEach(x => Console.WriteLine(x));
+
+            // Search Functions:
+            list.Sort();
+            Console.WriteLine(Search.Binary(list, 1));
+            Console.WriteLine(Search.Binary(list, 2));
+            Console.WriteLine(Search.Linear(list, 1));
+            Console.WriteLine(Search.Linear(list, 2));
         }
     }
 
@@ -58,6 +65,38 @@ namespace Lists
                 if (n < 0) values.Add(n);
             }
             return values;
+        }
+    }
+
+    class Search
+    {
+        static public bool Linear(List<int> list, int item)
+        {
+            foreach (int n in list)
+            {
+                if (n == item) return true;
+            }
+            return false;
+        }
+
+        static public bool Binary(List<int> list, int item)
+        {
+            int min = 0;
+            int max = list.Count - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (item < list[mid])
+                {
+                    max = mid - 1;
+                }
+                else if (item > list[mid])
+                {
+                    min = mid + 1;
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
