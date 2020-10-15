14837701f343be966c40e6123bf26a8f285ec375
dhruvpattem <dhruv.pattem@gmail.com>
2020-10-15 15:35:18 +0100

    made it

---
+++ Exceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Sqrt());
+        }
+
+        static public double Sqrt()
+        {
+            Console.Write("Number: ");
+            try
+            {
+                double x = double.Parse(Console.ReadLine());
+                return Math.Sqrt(x);
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("No");
+            }
+            finally
+            {
+            }
+        }
+    }
+}
---
+++ Questions/Question6-2517/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Question6_2517
+{
+    class Program
+    {
+        static void Main()
+        {
+            // TaskOne();
+            // TaskTwo();
+            TaskExtra();
+        }
+
+        static void TaskOne()
+        {
+            Console.Write("Task 1, Enter decimal number: ");
+            int n = int.Parse(Console.ReadLine());
+            while (n >= 1)
+            {
+                Console.WriteLine(n % 2);
+                n /= 2;
+            }
+        }
+
+        static void TaskTwo()
+        {
+            Console.Write("Task 2, Enter decimal number: ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> bin = new List<int>();
+            while (n >= 1)
+            {
+                bin.Add(n % 2);
+                n /= 2;
+            }
+            bin.Reverse();
+            Console.WriteLine(String.Join("", bin));
+        }
+
+        static void TaskExtra()
+        {
+            Console.Write("Task Extra, Enter decimal number (-127 to 0): ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 0)
+            {
+                n += 128;
+                List<int> bin = new List<int>();
+                while (n >= 1)
+                {
+                    bin.Add(n % 2);
+                    n /= 2;
+                }
+                Console.Write(1);
+                bin.Reverse();
+                string binstr = String.Join("", bin);
+                Console.WriteLine(binstr.PadLeft(7, '0'));
+            }
+            else
+            {
+                Console.WriteLine("00000000");
+            }
+        }
+    }
+}
