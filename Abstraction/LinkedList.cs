a82d2787dfc91de34965fc58dffe7f73ef177422
dhruvpattem <dhruv.pattem@gmail.com>
2021-02-12 12:28:38 +0000

    edits

--- Abstraction/LinkedList.cs
+++ Abstraction/LinkedList.cs
@@ -1,11 +1,22 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
-class LinkedList<T>
+class LinkedList<T> : IList<T>
 {
     private Node head;
     private Node tail;
-    public int Count = 0;
+    public int Count { get; private set; }
+
+    // not implemented yet
+    public bool IsReadOnly { get; }
+
+    public LinkedList()
+    {
+        Count = 0;
+        head = null;
+        tail = null;
+    }
 
     private class Node
     {
@@ -16,6 +27,21 @@ class LinkedList<T>
             this.item = item;
             this.next = next;
         }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node node = head;
+        for (int i = 0; i < Count; i++)
+        {
+            yield return node.item;
+            node = node.next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 
     public void Add(T item)
@@ -34,46 +60,143 @@ class LinkedList<T>
         Count++;
     }
 
-    public void RemoveAt(int index)
+    public bool Remove(T item)
     {
-        if (index == 0)
+        Node node = head;
+        if (EqualityComparer<T>.Default.Equals(item, head.item))
         {
-            this.head = this.head.next;
+            head = head.next;
+        }
+        for (int i = 0; i < Count - 1; i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(item, node.next.item))
+            {
+                node.next = node.next.next;
+                Count--;
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    public void RemoveAt(int idx)
+    {
+        if (idx >= Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        if (idx == 0)
+        {
+            head = head.next;
         }
         else
         {
-            Node node = this.head;
-            for (int i = 0; i < index - 1; i++)
+            Node node = head;
+            for (int i = 0; i < idx - 1; i++)
             {
                 node = node.next;
-                if (node == null)
-                {
-                    throw new ArgumentNullException("Index out of range");
-                }
             }
             node.next = node.next.next;
-            if (node == null)
-            {
-                tail = null;
-            }
         }
         Count--;
     }
 
-    public T this[int index]
+    public void Insert(int idx, T item)
+    {
+        if (idx >= Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        if (idx == 0)
+        {
+            Node node = new Node(item, next: head);
+            head = node;
+        }
+        else
+        {
+            Node node = head;
+            for (int i = 0; i < idx - 1; i++)
+            {
+                node = node.next;
+            }
+            node.next = new Node(item, next: node.next);
+        }
+        Count++;
+    }
+
+    public void Clear()
+    {
+        head = null;
+        tail = null;
+        Count = 0;
+    }
+
+    public void CopyTo(T[] arr, int idx)
+    {
+        Node node = head;
+        for (int i = idx; i < Count; i++)
+        {
+            arr[i] = node.item;
+            node = node.next;
+        }
+    }
+
+    public bool Contains(T item)
+    {
+        Node node = head;
+        for (int i = 0; i < Count; i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(item, node.item))
+            {
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    public int IndexOf(T item)
+    {
+        Node node = head;
+        for (int i = 0; i < Count; i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(item, node.item))
+            {
+                return i;
+            }
+            node = node.next;
+        }
+        return -1;
+    }
+
+    public T this[int idx]
     {
         get
         {
+            if (idx >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
             Node node = this.head;
-            for (int i = 0; i < index; i++)
+            for (int i = 0; i < idx; i++)
             {
                 node = node.next;
-                if (node == null)
-                {
-                    throw new ArgumentNullException("Index out of range");
-                }
             }
             return node.item;
+        }
+        set
+        {
+            if (idx >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            Node node = this.head;
+            for (int i = 0; i < idx; i++)
+            {
+                node = node.next;
+            }
+            node.item = value;
         }
     }
 }
